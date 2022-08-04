24a27b979979431237cc020ff99ee0b77a3f7882
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 17:40:34 -0700

    First three exercises passing.

--- exercise_2/exercise-2.sql
+++ exercise_2/exercise-2.sql
@@ -1,2 +1,4 @@
 -- find all rental dates for customers with the first_name Patricia
 -- use INNER JOIN FROM customer onto rental
+
+select rental_date from customer inner join rental on customer.customer_id = rental.customer_id where customer.first_name = 'Patricia' 
--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,8 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+
+select title from film 
+left join film_category on film.film_id = film_category.film_id
+left join category on category.category_id = film_category.category_id
+where category.name = 'Action';
