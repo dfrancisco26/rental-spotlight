69d1e882f9ab76d62795fb4d7197e621562a3340
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 18:54:10 -0700

    exercises 12 and 13 completed. attempted 14, gave up.

--- exercise_12/exercise_12.sql
+++ exercise_12/exercise_12.sql
@@ -3,4 +3,12 @@
 -- use SUM to sum up the amounts
 
 
-select city, count()
+select city, sum(payment.amount) from city
+left join address on city.city_id = address.city_id
+left join customer on address.address_id = customer.address_id
+left join rental on customer.customer_id = rental.customer_id
+left join payment on rental.rental_id = payment.rental_id
+where payment.amount is not null
+group by city.city
+order by sum(payment.amount) desc
+limit 10
--- exercise_13/exercise_13.sql
+++ exercise_13/exercise_13.sql
@@ -1,3 +1,13 @@
 -- the average rental amount by country
 -- include the country name and avg amount
 -- use AVG to average the amount
+select country, avg(payment.amount) from country
+left join city on country.country_id = city.country_id
+left join address on city.city_id = address.city_id
+left join customer on address.address_id = customer.address_id
+left join rental on customer.customer_id = rental.customer_id
+left join payment on rental.rental_id = payment.rental_id
+where payment.amount is not null
+group by country.country
+order by avg(payment.amount) desc
+limit 10
