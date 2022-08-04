b52285293fb99a2caaa25225cbc15a0f109beb5f
David Francisco <dfrancisco2677@gmail.com>
2022-08-03 18:05:14 -0700

    Tests 4, 5, 6 pass.

--- exercise_4/exercise-4.sql
+++ exercise_4/exercise-4.sql
@@ -1,2 +1,6 @@
 -- find all customers first_name, last_name, address, and city
 -- Similar to exercise 3 but with customer, address, city
+
+select first_name, last_name, address, city from customer
+left join address on customer.address_id = address.address_id
+left join city on address.city_id = city.city_id
--- exercise_5/exercise-5.sql
+++ exercise_5/exercise-5.sql
@@ -1,2 +1,7 @@
 -- find all film titles that an actor with the last_name Davis appears in
 -- Similar to exercise 3 but with actor, film_actor, film
+
+select title from actor 
+left join film_actor on actor.actor_id = film_actor.actor_id
+left join film on film_actor.film_id = film.film_id
+where actor.last_name = 'Davis'
